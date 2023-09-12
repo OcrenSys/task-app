2121f969c85de5891fe92aa3f3b0e74cc28fe04e
Jairo Martinez <ocrensys@gmail.com>
2023-09-12 11:11:58 -0600

    implement list action service

--- src/app/core/components/task-list/task-list.component.ts
+++ src/app/core/components/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { Task } from 'src/app/shared/types/Task';
 import { MatDialog } from '@angular/material/dialog';
@@ -6,6 +6,8 @@ import { TaskFormComponent } from '../task-form/task-form.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { TaskService } from '../../services/task/task.service';
+import { tap, takeUntil, Subject } from 'rxjs';
 
 const TASK_DATA: Task[] = [
   {
@@ -48,11 +50,31 @@ const TASK_DATA: Task[] = [
   standalone: true,
   imports: [MatTableModule, MatButtonModule, MatIconModule, MatCheckboxModule],
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit, OnDestroy {
   displayedColumns: string[] = ['title', 'description', 'status', 'option'];
   dataSource = TASK_DATA;
+  protected stop$ = new Subject();
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    // this.getList();
+  }
+
+  ngOnDestroy(): void {
+    this.stop$.next(null);
+    this.stop$.complete();
+  }
+
+  private getList(): void {
+    this.taskService
+      .list()
+      .pipe(
+        takeUntil(this.stop$),
+        tap((response) => console.log(response))
+      )
+      .subscribe();
+  }
 
   protected onClick(row: Task) {
     const dialogRef = this.dialog.open(TaskFormComponent, {
