a4b3395f431fef75cf1ad4f31f5dab39a9c26459
Jairo Martinez <ocrensys@gmail.com>
2023-09-13 21:18:21 -0600

    reformat server url

--- src/app/shared/constants/urls.constants.ts
+++ src/app/shared/constants/urls.constants.ts
@@ -1,4 +1,3 @@
 export const TASK_MODEL: string = 'tasks';
 export const SERVER_DOMAIN: string =
-  'https://us-central1-taskapp-8ae9b.cloudfunctions.net';
-export const API_V1: string = 'api';
+  'https://us-central1-taskapp-8ae9b.cloudfunctions.net/api';
